46c0b04a593be285c956e988ea8f1be1659935dc
MicahMartin <micah.m329@gmail.com>
2024-07-04 17:32:14 -0400

    ok

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,13 @@
 // import './App.css'
 import Header from './components/Header.jsx'
+import NavBar from './components/NavBar.jsx'
 
 const outerClassName = 'w-full max-w-[60rem] min-h-screen mx-auto border-x border-dashed border-[#40e07d]/30 flex gap-4 py-4 flex-col text-[#40e07d]'
 function App() {
   return (
     <div className={outerClassName}>
     <Header/>
+    <NavBar/>
     </div>
   )
 }
---
+++ frontend/src/components/NavBar.jsx
@@ -0,0 +1,19 @@
+const NavBar = (props) => {
+  return(
+    <div className="max-w-full text-sm bg-[#40e07d]/75 text-black">
+    <ul className="flex justify-between">
+    <li className="m-auto">
+      projects
+    </li>
+    <li className="m-auto">
+      blog
+    </li>
+    <li className="m-auto">
+      about / contact
+    </li>
+    </ul>
+    </div>
+  )
+
+}
+export default NavBar
